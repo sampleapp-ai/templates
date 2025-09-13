9f000c7d16a9d1b35864cb8e13ff953cec99a887
Sean Er <seanerwenhan@gmail.com>
2025-09-13 21:53:34 +0800

    Add chatbar to vite app

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import {ChatBar} from "@sampleapp.ai/sdk";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -17,19 +15,22 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div style={{width: "800px", margin: "0 auto"}}>
+        <ChatBar
+          playgroundUid="yourCompanyName"
+          height="auto"
+          typingTexts={[
+            "How do I use yourCompanyName?",
+            "What is yourCompanyName?",
+            "Build with yourCompanyName",
+          ]}
+        />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
